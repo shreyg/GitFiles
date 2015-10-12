f27fe9dbdad4824f6bac9b6b2c5775bcfa6b0afd
meetvaruni <meetvaruni@gmail.com>
2015-10-12 16:58:55 -0700

    Updated files

--- stanfordPOScmnds.py
+++ stanfordPOScmnds.py
@@ -1,4 +1,6 @@
 from nltk.tag import StanfordPOSTagger
 st = StanfordPOSTagger("models\\english-bidirectional-distsim.tagger",path_to_jar="stanford-postagger.jar")
-print(st.tag([u'paid', u'double', u'what', u'i', u'found', u'at', u'another', u'site', u'outside', u'of', u'amazon', u'/', u'this', u'store', u'-', u'never', u'again', u'!']))
+#print(st.tag([u'paid', u'double', u'what', u'i', u'found', u'at', u'another', u'site', u'outside', u'of', u'amazon', u'/', u'this', u'store', u'-', u'never', u'again', u'!']))
 print(st.tag("These sunglasses are all right . They were a little crooked but is still cool..".split()))
+print(st.tag([u'These',u'sunglasses',u'are',u'all',u'right',u'They',u'were',u'a',u'little',u'crooked',u'but',u'is',u'still',u'cool']))
+print(st.tag([u'These',u'sunglasses',u'are',u'all',u'right']))
