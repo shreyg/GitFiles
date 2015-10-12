d49322b293ea7f3ecd182420507fd57e459cfac2
meetvaruni <meetvaruni@gmail.com>
2015-10-12 10:32:57 -0700

    latest one

--- stanfordPOScmnds.py
+++ stanfordPOScmnds.py
@@ -1,4 +1,4 @@
 from nltk.tag import StanfordPOSTagger
 st = StanfordPOSTagger("models\\english-bidirectional-distsim.tagger",path_to_jar="stanford-postagger.jar")
 print(st.tag([u'paid', u'double', u'what', u'i', u'found', u'at', u'another', u'site', u'outside', u'of', u'amazon', u'/', u'this', u'store', u'-', u'never', u'again', u'!']))
-print(st.tag("paid double what i found at another site outside of amazon/this store - NEVER AGAIN!".split()))
+print(st.tag("These sunglasses are all right . They were a little crooked but is still cool..".split()))
